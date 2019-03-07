d496e4235130e0eb97f50f7340e3ffd23ae47e2b
CatrinaH <caricokid@gmail.com>
2019-03-07 15:40:44 -0700

    changed code in Stopwatch Activity so buttons work.

--- app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -2,6 +2,9 @@ package com.hfad.stopwatch;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import java.util.Locale;
+import android.os.Handler;
+import android.widget.TextView;
 public class StopwatchActivity extends Activity {
     private int seconds = 0;
     private boolean running;
@@ -9,6 +12,7 @@ public class StopwatchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
     }
     //Start the stopwatch running when the Start button is clicked.
     public void onClickStart(View view) {
@@ -22,5 +26,25 @@ public class StopwatchActivity extends Activity {
     public void onClickReset(View view) {
         running = false;
         seconds = 0;
+    }
+    //Sets the number of seconds on the timer.
+    private void runTimer() {
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
